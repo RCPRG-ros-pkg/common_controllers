c1a176f629a60ff24081000dbe6fb9453c124035
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-10-20 10:25:16 +0200

    improved tf publisher: do not publish invalid transforms

--- src/tf_publisher.cpp
+++ src/tf_publisher.cpp
@@ -1,4 +1,30 @@
-// Copyright 2014 WUT
+/*
+ Copyright (c) 2014, Robot Control and Pattern Recognition Group,
+ Warsaw University of Technology
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the Warsaw University of Technology nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYright HOLDER> BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 /*
  * tf_publisher.cpp
  *
@@ -38,7 +64,7 @@ bool TfPublisher::configureHook() {
     return false;
   }
 
-  message_.transforms.resize(N_);
+  message_.transforms.reserve(N_);
   port_in_.resize(N_);
 
   for (size_t i = 0; i < N_; i++) {
@@ -48,14 +74,6 @@ bool TfPublisher::configureHook() {
     this->ports()->addPort(name, *port_in_[i]);
   }
 
-  pose_.position.x = 0;
-  pose_.position.y = 0;
-  pose_.position.z = 0;
-  pose_.orientation.x = 0;
-  pose_.orientation.y = 0;
-  pose_.orientation.z = 0;
-  pose_.orientation.w = 0;
-
   return true;
 }
 
@@ -64,16 +82,22 @@ bool TfPublisher::startHook() {
 }
 
 void TfPublisher::updateHook() {
+  size_t tf_count = 0;
+  message_.transforms.resize(N_);   // the space was reserved in configureHook
   for (size_t i = 0; i < N_; i++) {
-    port_in_[i]->readNewest(pose_);
-    message_.transforms[i].header.stamp = rtt_rosclock::host_now();
-    message_.transforms[i].header.frame_id = frame_ids_[i];
-    message_.transforms[i].child_frame_id = child_frame_ids_[i];
-    message_.transforms[i].transform.translation.x = pose_.position.x;
-    message_.transforms[i].transform.translation.y = pose_.position.y;
-    message_.transforms[i].transform.translation.z = pose_.position.z;
-    message_.transforms[i].transform.rotation = pose_.orientation;
+    geometry_msgs::Pose pose;
+    if (port_in_[i]->read(pose) == RTT::NewData) {
+      message_.transforms[tf_count].header.stamp = rtt_rosclock::host_now();
+      message_.transforms[tf_count].header.frame_id = frame_ids_[i];
+      message_.transforms[tf_count].child_frame_id = child_frame_ids_[i];
+      message_.transforms[tf_count].transform.translation.x = pose.position.x;
+      message_.transforms[tf_count].transform.translation.y = pose.position.y;
+      message_.transforms[tf_count].transform.translation.z = pose.position.z;
+      message_.transforms[tf_count].transform.rotation = pose.orientation;
+      ++tf_count;
+    }
   }
+  message_.transforms.resize(tf_count);     // the space was reserved in configureHook
 
   port_out_tf_.write(message_);
 }
